d96959b4e5384aaa842c608046c2572fe80c2caf
tsmckinney <tsmckinney10@gmail.com>
2023-07-16 16:43:26 -0400

    Added notice in joke list and updated code

--- montyPython/monty.py
+++ montyPython/monty.py
@@ -1,13 +1,13 @@
 import random
-jokes = open("montyPython/jokeList.txt", "r")
+jokes = open("jokeList.txt", "r")
 joke = jokes.readlines()
 
 jokeList = []
 jokeIDX = 0
 for j in joke:
     jokeList.append(j.strip())
-    jokeIDX+=1
-jokeNum = random.randint(0,jokeIDX)
+    jokeIDX = jokeIDX + 1
+jokeNum = random.randint(1, jokeIDX)
 if jokeNum < 1:
     jokeNum = 1
 elif jokeNum > jokeIDX:
